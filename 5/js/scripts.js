e7b8c874a699f963b2675f16c84d38ac03a0dba3
Keks <keks@htmlacademy.ru>
2023-04-18 22:49:23 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/scripts.js
+++ 5/js/scripts.js
@@ -1,6 +1,9 @@
 const menuButtonToggle = document.querySelector('.menu__toggle');
 const menuList = document.querySelector('.menu__list');
-const userList = document.querySelector('.user__list')
+const userList = document.querySelector('.user__list');
+const navigation = document.querySelector('nav');
+
+navigation.classList.remove('header__menu--nojs');
 
 menuButtonToggle.addEventListener('click', () => {
   let expanded = menuButtonToggle.getAttribute('aria-expanded') === 'true';
