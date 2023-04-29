beae7f16493ef2f34e0d05ec9165db3111cf2256
Yaroslav <yibelyaev@icloud.com>
2023-04-29 19:35:46 +0300

    fix error

--- gulpfile.js
+++ gulpfile.js
@@ -132,7 +132,7 @@ const reload = (done) => {
 const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
   gulp.watch('source/js/script.js', gulp.series(scripts));
-  gulp.watch('source/*.html').on('change', browser.reload);
+  gulp.watch('source/*.html', gulp.series(html, reload))
   gulp.watch('source/img/icons/*.svg').on('change', svg, browser.reload);
 }
 
